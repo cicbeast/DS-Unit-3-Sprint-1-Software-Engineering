a589d324a317973d40032409bb5635ff9a5d3083
cicbeast <49789241+cicbeast@users.noreply.github.com>
2019-09-14 00:43:39 -0400

    Updated acme and acme_report

--- Unit-3-SC1/acme.py
+++ Unit-3-SC1/acme.py
@@ -1,30 +1,49 @@
 import unittest
 import random
 
+
 class Product():
-    def __init__(self, name, price=10, weight=20, flammability=0.5, identifier=random.randint(1000000,9999999)):
+    def __init__(self, name,
+                 price=10,
+                 weight=20,
+                 flammability=0.5,
+                 identifier=random.randint(1000000, 9999999)):
         self.name = name
         self.price = price
         self.weight = weight
         self.flammability = flammability
         self.identifier = identifier
-    
+
     def stealability(self):
-        '''Uses the quotient of price & weight to calculate how 'stealable' an product is'''
+        '''Uses the quotient of price & weight
+        to calculate how 'stealable' an product is'''
         q = self.price / self.weight
-        if q < 0.5: print('Not so stealable...')
-        elif 0.5 <= q < 1: print('Kinda Stealable.')
-        else: print('Very Stealable')
-    
+        if q < 0.5:
+            print('Not so stealable...')
+        elif 0.5 <= q < 1:
+            print('Kinda Stealable.')
+        else:
+            print('Very Stealable')
+
     def explode(self):
-        '''Uses the product of flammability & weight to calculate how likely it is that the product will blow up'''
+        '''Uses the product of flammability & weight
+            to calculate likelyhood of explosion'''
         p = self.flammability * self.weight
-        if p < 10: print('...fizzle.')
-        elif 10 <= p < 50: print('...boom!')
-        else: print('...BABOOM!!')
+        if p < 10:
+            print('...fizzle.')
+        elif 10 <= p < 50:
+            print('...boom!')
+        else:
+            print('...BABOOM!!')
+
 
 class BoxingGlove(Product):
-    def __init__(self, name, price=10, weight=10, flammability=0.5, identifier=random.randint(1000000,9999999)):
+    def __init__(self,
+                 name,
+                 price=10,
+                 weight=10,
+                 flammability=0.5,
+                 identifier=random.randint(1000000, 9999999)):
         self.name = name
         self.price = price
         self.weight = weight
@@ -35,6 +54,9 @@ class BoxingGlove(Product):
         print("...it's a glove.")
 
     def punch(self):
-        if self.weight < 5: print('That Tickles.')
-        elif 5 <= self.weight < 15: print('Hey that hurt!')
-        else: print('OUCH!')
+        if self.weight < 5:
+            print('That Tickles.')
+        elif 5 <= self.weight < 15:
+            print('Hey that hurt!')
+        else:
+            print('OUCH!')
--- Unit-3-SC1/acme_report.py
+++ Unit-3-SC1/acme_report.py
@@ -4,25 +4,38 @@ from random import randint, sample, uniform
 adj = ['Awesome', 'Shiny', 'Impressive', 'Portable', 'Improved']
 noun = ['Anvil', 'Catapult', 'Disguise', 'Mousetrap', '???']
 
-def generate_products(num = 30):
+
+def generate_products(num=30):
     products = []
     n = 0
     while n < num:
-        P = Product(name=[sample(adj,1)+sample(noun,1)],
-                                      price=randint(5, 100),
-                                      weight=randint(5, 100),
-                                      flammability=uniform(0.0, 2.5),
-                                      identifier=randint(1000000,9999999)
-                                      )
-        P_info = [P.name, P.price, P.weight, P.flammability, P.identifier]
+        P = Product(name=str(sample(adj, 1)+sample(noun, 1))
+                    price=randint(5, 100),
+                    weight=randint(5, 100),
+                    flammability=uniform(0.0, 2.5),
+                    identifier=randint(1000000, 9999999)
+                    )
+        P_info = [[P.name, P.price, P.weight, P.flammability, P.identifier]]
         products = products + P_info
         n += 1
-    # print(products)
     return products
 
+
 def inventory_report(products):
-    # num_unique = 0
-    pass
+    prod_names = [row[0] for row in products]
+    prod_price = [row[1] for row in products]
+    prod_weight = [row[2] for row in products]
+    prod_flam = [row[3] for row in products]
+    num_unique = len(set(prod_names))
+    av_price = sum(prod_price)/len(prod_price)
+    av_weight = sum(prod_weight)/len(prod_weight)
+    av_flam = sum(prod_flam)/len(prod_flam)
+    print('ACME CORPORATION OFFICIAL INVENTORY REPORT')
+    print('Number of Unique Product Names:', num_unique)
+    print('Average Price of Products:', av_price)
+    print('Average Weight of Products:', av_weight)
+    print('Average Flammability of Products:', av_flam)
+
 
 if __name__ == '__main__':
     inventory_report(generate_products())
